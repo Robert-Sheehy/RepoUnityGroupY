53b6b1079e9e8c55755299119e782eadc40a1b85
LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>
2024-10-24 10:59:41 +0100

    Signed-off-by: LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>

---
+++ RADY/Assets/explodeScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explodeScript : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(10 * transform.forward,ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
