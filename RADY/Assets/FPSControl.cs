947942aabd2a587e5ba88de0b8bccf0a1f36139b
LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>
2024-11-06 12:53:34 +0000

    Signed-off-by: LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>

--- RADY/Assets/FPSControl.cs
+++ RADY/Assets/FPSControl.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class FPSControl : MonoBehaviour
 {
+    public GameObject knifeCloneTemplate;
     float speed = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+     Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += speed* transform.forward * Time.deltaTime;
+        {   Vector3 fpsMovementDir = new Vector3(transform.forward.x, 0, transform.forward.z);
+            fpsMovementDir.Normalize();
+            transform.position += speed* fpsMovementDir * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position -= speed * transform.forward * Time.deltaTime;
+            Vector3 fpsMovementDir = new Vector3(transform.forward.x, 0, transform.forward.z);
+            fpsMovementDir.Normalize();
+            transform.position -= speed * fpsMovementDir * Time.deltaTime;
         }
 
-        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal"));
-        transform.Rotate(Vector3.right, Input.GetAxis("Vertical"));
+        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal"), Space.World);
+        transform.Rotate(transform.right, Input.GetAxis("Vertical"),Space.World);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(knifeCloneTemplate, transform.position, transform.rotation);
+        }
     }
 }
